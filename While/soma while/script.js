8d4f9d2ff371b665783ee8a3e148cab3eab3e11f
Kalel-2009 <kalelantunes2009@gmail.com>
2026-05-05 08:23:21 -0300

    Add files via upload

---
+++ While/soma while/script.js
@@ -0,0 +1,29 @@
+    var x = 0;
+    var soma = 0
+while(x<=5) {
+
+    soma = soma + x;
+
+    x++
+
+}
+console.log(soma);
+
+
+// Array //
+var arr = ['teste1', 'teste2', 'teste3', 'a' ];
+
+var y = 0
+
+while(y <= 3) {
+    console.log(arr[y]);
+    y++
+}
+
+// Repete palavras //
+var palavra = "Thiago";
+var i = 0;
+while(i < 6){
+    console.log(palavra[i]);
+    i++
+}
